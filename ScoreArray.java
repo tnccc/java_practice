4a070277e9710b44e08430425f06fa73e3c8edaf
tnccc <irei.frontend@gmail.com>
2023-08-24 12:50:27 +0900

    test

---
+++ ScoreArray.java
@@ -0,0 +1,13 @@
+class ScoreArray {
+  public static void main(String[] args) {
+    int[] data = new int[4];
+    data[0] = 90;
+    data[1] = 80;
+    data[2] = 100;
+    data[3] = 90;
+    int[]  data2 = {90, 80, 100, 90};
+
+    System.out.println(data[0]);
+    System.out.println(data2[3]);
+  }
+}
