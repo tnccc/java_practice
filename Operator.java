e65a31928d2ba81fdd7ae8e1e1d46207b5bc55e3
tnccc <irei.frontend@gmail.com>
2023-08-24 17:15:54 +0900

    practice

---
+++ Operator.java
@@ -0,0 +1,8 @@
+class Operator {
+  public static void main(String[] args) {
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+
+    System.out.println(a + b);
+  }
+}
