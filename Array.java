c7b13477dde17017b2c715f622809252023f0888
tnccc <irei.frontend@gmail.com>
2023-08-24 11:07:28 +0900

    Array

---
+++ Array.java
@@ -0,0 +1,16 @@
+class Array {
+  public static void main(String[] args) {
+    // 宣言 => 生成 => 代入
+    // int[] score = new int[3];
+    // score[0] = 80;
+    // score[1] = 100;
+    // score[2] = 75;
+
+    // 配列の初期化
+    String[] name = {"菅原", "村山", "寺島"};
+    int[] score = {80, 100, 75};
+
+    System.out.println(score[0]);
+    System.out.println(score.length);
+  }
+}
